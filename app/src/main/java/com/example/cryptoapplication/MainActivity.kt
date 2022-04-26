61ac604fdf53204f5e200a3f6f57a744e8c6cc9d
kocerenes <eneskocerr41@gmail.com>
2022-04-27 00:31:28 +0300

    Create splash screen and update color update at screens.

--- app/src/main/java/com/example/cryptoapplication/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
